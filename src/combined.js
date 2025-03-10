be8f3a978c80384c8a4fa4d3a021483aa322c86a
samuelitwaru <samuelitwaru@gmail.com>
2025-03-10 15:38:32 +0300

    more

--- src/combined.js
+++ src/combined.js
@@ -282,7 +282,7 @@ class LoadingManager {
 }
 
 // Content from classes/DataManager.js
-const environment = "/ComfortaKBDevelopmentNETSQLServer";
+const environment = "/Comforta_version2DevelopmentNETPostgreSQL";
 const baseURL = window.location.origin + (window.location.origin.startsWith("http://localhost") ? environment : "");
 
 class DataManager {

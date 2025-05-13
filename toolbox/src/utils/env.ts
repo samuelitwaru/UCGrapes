38c337bf925e086e272dd4bae97014df6d269874
Mark Bright Baraka <markbrightbaraka@email.com>
2025-05-13 13:22:20 +0300

    update gitignore

--- toolbox/src/utils/env.ts
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-export const environment = "/ComfortaKBDevelopmentNETSQLServer";
-=======
-export const environment = "/Comforta_version2_Development7NETPostgreSQL3";
->>>>>>> d5d0f4ef302218dc2988ace4c2bdfed81d7d8317
